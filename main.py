56daa73deb247c1d2597764c188aae9fc30daa82
sidney2k1 <sidsingla02@gmail.com>
2025-01-05 11:50:02 +0530

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+#program to showcase working of different types of bitwise operations
+num1=10
+num2=4
+print("num1 and num2=",num1 & num2)
+print("\nnum1|num2=",num1|num2)
+print("\n~num1=",~num1)
+print("\nnum1^num2=",num1^num2)
+num1=10
+num2=4
+print("\nnum1 >>1=",num1>>1)
+print("\nnum2<<1=",num2<<1)
+num1=10
+num2=4
+print("\nnum<<1=",num1<<1)
+print("\nnum2>>1=",num2>>1)
