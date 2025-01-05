ce9a55f55ca03ef003dd12cb88e7ca800ab02236
sidney2k1 <sidsingla02@gmail.com>
2025-01-05 13:20:04 +0530

    project

---
+++ project.py
@@ -0,0 +1,14 @@
+num1=150
+num2=30
+print("num1 and num2=",num1 & num2)
+print("\nnum1|num2=",num1|num2==num1&num2)
+print("\n~num1=",~num1)
+print("\nnum1^num2=",num1^num2+~num1)
+num1=19
+num2=200
+print("\nnum1 >>1=",num1>>2)
+print("\nnum2<<1=",num2<<3)
+num1=10
+num2=10
+print("\nnum<<1=",num1<<10)
+print("\nnum2>>1=",num2>>9)
